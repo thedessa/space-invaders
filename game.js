70616c261d265eb2df7035158e74297593e7a48c
Andressa Ruy <ruy.andressa@gmail.com>
2021-08-05 20:03:42 -0300

    phaser working better

--- game.js
+++ game.js
@@ -1,3 +1,27 @@
-import Phaser from 'phaser';
+var config = {
+    type: Phaser.AUTO, // render mode
+    parent: "game", // div id
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-new Phaser.Game();
+var spaceInvaders = new Phaser.Game(config);
+
+function preload() {
+    // load images and audio
+}
+
+
+function create() {
+    // create scenes, characters, text
+}
+
+
+function update() {
+    // update game elements
+}
